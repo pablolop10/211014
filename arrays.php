f9ca0d1df4a71a3d67cec4300a330d2fc55abede
pablolop10 <pablomoreda7@gmail.com>
2021-10-14 09:55:05 +0200

    commit ordenador

--- arrays.php
+++ arrays.php
@@ -1,3 +1,16 @@
 <?php
-echo 'hola'
- ?>
+
+$datos["71779337H"]="Pablo García López";
+$datos["71723451A"]="Paula Martinez Diaz";
+$datos["71779541V"]="Pepe Suarez López"; 
+
+
+
+
+foreach($datos as $dni=>$nombre){
+  echo "La persona con dni: $dni tiene de nombre: $nombre";
+  echo "<br>";
+}
+
+
+?>
--- readme.txt
+++ readme.txt
@@ -1,1 +1,1 @@
-hola mundo
+hola mundo desde visual studio
